481be2e135c78740680bac2bdf5a06136476e005
mikegamboa <23.mikegamboa@gmail.com>
2025-09-12 16:38:32 +0800

    update db and fields

--- apps/home/models.py
+++ apps/home/models.py
@@ -1,12 +1,54 @@
 from apps import db
+from datetime import date
 
 class Registro(db.Model):
     __tablename__ = 'registro'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    phone = db.Column(db.String(20), nullable=False)
+    #int_col_primary_key = db.Column(db.Integer, primary_key=True)
+    #string_col_not_nullable = db.Column(db.String(120), nullable=False)
+    #string_col_nullable = db.Column(db.String(120), nullable=True)
+    #string_col_unique = db.Column(db.String(120), unique=True, nullable=False)
+    #string_col_not_unique = db.Column(db.String(120), unique=False, nullable=False)
+    #date_col = db.Column(db.Date)
+    #bool_col = db.Column(db.Boolean, default=False) 
+    #timestamp_col = 
+    
+    servici = db.Column(db.String(20))
+    jo_no = db.Column(db.Integer, primary_key=True)
+    date_in = db.Column(db.Date)
+    date_out = db.Column(db.Date)
+    vin = db.Column(db.String(17), unique=True, nullable=False)
+    make = db.Column(db.String(100))
+    model = db.Column(db.String(100))
+    plate_no = db.Column(db.String(100))
+    color = db.Column(db.String(100))
+    notes = db.Column(db.String(200))
+    cliente = db.Column(db.String(100))
+    tagged = db.Column(db.Boolean, default=False) 
+    paperwork = db.Column(db.Boolean, default=False) 
+    diagnosis = db.Column(db.String(100))
+    estimate = db.Column(db.String(100))
+    parts_and_materials = db.Column(db.String(100))
+    billing = db.Column(db.String(100))
+    posizione = db.Column(db.String(100))
+    day_count = db.Column(db.Integer)
+    released_to = db.Column(db.String(100))
+    received_from = db.Column(db.String(100))
+    related_jo = db.Column(db.Integer)
+    received_by = db.Column(db.String(100))
+    date_checklisted = db.Column(db.Date)
+    checklisted_by = db.Column(db.String(100))
+    released_by = db.Column(db.String(100))
+    km_mileage = db.Column(db.Integer)
+    concerns_requests = db.Column(db.String(200))
+    scartoffie = db.Column(db.Integer)
+    transazioni = db.Column(db.Integer)
+    created = db.Column(db.String(100))
+    last_edited = db.Column(db.String(100))
+    created_by = db.Column(db.String(100))
+    last_edited_by = db.Column(db.String(100))
+    checklist = db.Column(db.String(100))
+    onsite_details = db.Column(db.String(100))
 
     def __repr__(self):
         return f'<Registro {self.name}>'
