7cec12d5c03c7fd60599d654cdc9be37506511a3
mikegamboa <23.mikegamboa@gmail.com>
2025-09-22 11:06:09 +0800

    Clean add and edit functionality

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -34,53 +34,46 @@ def registro():
         created_str = request.form.get("created")
         last_edited_str = request.form.get("last_edited")
 
-        #name = request.form.get("name")
         servici = request.form.get("servici")
         jo_no = request.form.get("jo_no")
-        #date_in = request.form.get("date_in").strftime("%m/%d/%Y")
-        #date_out = request.form.get("date_out").strftime("%m/%d/%Y")
-        #date_in = request.form.get("date_in")
-        #date_out = request.form.get("date_out")
         date_in = datetime.strptime(date_in_str, "%Y-%m-%d").date() if date_in_str else None
         date_out = datetime.strptime(date_out_str, "%Y-%m-%d").date() if date_out_str else None
         vin = request.form.get("vin")
+        plate_no = request.form.get("plate_no")
         make = request.form.get("make")
         model = request.form.get("model")
-        plate_no = request.form.get("plate_no")
         color = request.form.get("color")
+        km_mileage = request.form.get("km_mileage")
         notes = request.form.get("notes")
+        posizione = request.form.get("posizione")
         cliente = request.form.get("cliente")
+        concerns_requests = request.form.get("concerns_requests")
+        diagnosis = request.form.get("diagnosis")
+        onsite_details = request.form.get("onsite_details")
         tagged = request.form.get("tagged")
         paperwork = request.form.get("paperwork")
-        diagnosis = request.form.get("diagnosis")
         estimate = request.form.get("estimate")
         parts_and_materials = request.form.get("parts_and_materials")
         billing = request.form.get("billing")
-        posizione = request.form.get("posizione")
         day_count = request.form.get("day_count")
-        released_to = request.form.get("released_to")
         received_from = request.form.get("received_from")
-        related_jo = request.form.get("related_jo")
         received_by = request.form.get("received_by")
-        #date_checklisted = request.form.get("date_checklisted")
         date_checklisted = datetime.strptime(date_checklisted_str, "%Y-%m-%d").date() if date_checklisted_str else None
         checklisted_by = request.form.get("checklisted_by")
-        released_by = request.form.get("released_by")
-        km_mileage = request.form.get("km_mileage")
-        concerns_requests = request.form.get("concerns_requests")
+        checklist = request.form.get("checklist")
         scartoffie = request.form.get("scartoffie")
         transazioni = request.form.get("transazioni")
-        #created = request.form.get("created")
+        transazioni_intl = request.form.get("transazioni_intl")
+        related_jo = request.form.get("related_jo")
+        released_to = request.form.get("released_to")
+        released_by = request.form.get("released_by")
         created = datetime.strptime(created_str, "%Y-%m-%d %H:%M:%S") if created_str else datetime.now()
-        #last_edited = request.form.get("last_edited")
-        last_edited = datetime.strptime(last_edited_str, "%Y-%m-%d %H:%M:%S") if last_edited_str else datetime.now()
         created_by = request.form.get("created_by")
+        last_edited = datetime.strptime(last_edited_str, "%Y-%m-%d %H:%M:%S") if last_edited_str else datetime.now()
         last_edited_by = request.form.get("last_edited_by")
-        checklist = request.form.get("checklist")
-        onsite_details = request.form.get("onsite_details")
         
         # Validate required fields
-        if not (servici and jo_no and date_in and vin and make and model and plate_no and color and cliente and posizione and received_by):
+        if not (servici and jo_no and date_in and vin and plate_no and make and model and color and cliente and posizione and received_by):
             flash("Please fill required fields", "danger")
             return redirect(url_for("home_blueprint.registro"))
         
@@ -98,7 +91,46 @@ def registro():
             db.session.commit()
 
         # If unique, create new record
-        new_registro = Registro(servici=servici, jo_no=jo_no, date_in=date_in, date_out=date_out, vin=vin, make=make, model=model, plate_no=plate_no, color=color, notes=notes, cliente=cliente, tagged=tagged, paperwork=paperwork, diagnosis=diagnosis, estimate=estimate, parts_and_materials=parts_and_materials, billing=billing, posizione=posizione, day_count=day_count, released_to=released_to, received_from=received_from, related_jo=related_jo, received_by=received_by, date_checklisted=date_checklisted, checklisted_by=checklisted_by, released_by=released_by, km_mileage=km_mileage, concerns_requests=concerns_requests, scartoffie=scartoffie, transazioni=transazioni, created=created, last_edited=last_edited, created_by=created_by, last_edited_by=last_edited_by, checklist=checklist, onsite_details=onsite_details)
+        new_registro = Registro(
+            servici=servici, 
+            jo_no=jo_no, 
+            date_in=date_in, 
+            date_out=date_out, 
+            vin=vin, 
+            plate_no=plate_no, 
+            make=make, 
+            model=model, 
+            color=color, 
+            km_mileage=km_mileage, 
+            notes=notes, 
+            posizione=posizione,
+            cliente=cliente, 
+            concerns_requests=concerns_requests, 
+            onsite_details=onsite_details,
+            received_from=received_from, 
+            received_by=received_by, 
+            date_checklisted=date_checklisted, 
+            checklisted_by=checklisted_by, 
+            related_jo=related_jo, 
+            released_to=released_to, 
+            released_by=released_by,
+            # auto-fill 
+            tagged="No",
+            paperwork="No",
+            diagnosis="PDD (Pending Diagnosis)", 
+            estimate="Pending Estimate", 
+            parts_and_materials="PPS (Parts Procurement Stage)", 
+            billing="Pending Billing",  
+            day_count=(datetime.today().date()-date_in).days, 
+            checklist="None", 
+            scartoffie="None", 
+            transazioni="None", 
+            transazioni_intl="None", 
+            created=datetime.now(), 
+            created_by=created_by, 
+            last_edited=last_edited, 
+            last_edited_by=last_edited_by
+            )
 
         # Add to DB session
         db.session.add(new_registro)
@@ -135,13 +167,8 @@ def edit_registro(id):
         created_str = request.form.get("created")
         last_edited_str = request.form.get("last_edited")
 
-        #registro.name = request.form.get("name")
         registro.servici = request.form.get("servici")
         registro.jo_no = request.form.get("jo_no")
-        #registro.date_in = request.form.get("date_in").strftime("%m/%d/%Y")
-        #registro.date_out = request.form.get("date_out").strftime("%m/%d/%Y")
-        #registro.date_in = request.form.get("date_in")
-        #registro.date_out = request.form.get("date_out")
         registro.date_in = datetime.strptime(date_in_str, "%Y-%m-%d").date() if date_in_str else None
         registro.date_out = datetime.strptime(date_out_str, "%Y-%m-%d").date() if date_out_str else None
         registro.vin = request.form.get("vin")
@@ -151,7 +178,7 @@ def edit_registro(id):
         registro.color = request.form.get("color")
         registro.notes = request.form.get("notes")
         registro.cliente = request.form.get("cliente")
-        registro.tagged = bool(request.form.get("tagged"))
+        registro.tagged = request.form.get("tagged")
         registro.paperwork = request.form.get("paperwork")
         registro.diagnosis = request.form.get("diagnosis")
         registro.estimate = request.form.get("estimate")
@@ -163,7 +190,6 @@ def edit_registro(id):
         registro.received_from = request.form.get("received_from")
         registro.related_jo = request.form.get("related_jo")
         registro.received_by = request.form.get("received_by")
-        #registro.date_checklisted = request.form.get("date_checklisted").strftime("%m/%d/%Y")
         registro.date_checklisted = datetime.strptime(date_checklisted_str, "%Y-%m-%d").date() if date_checklisted_str else None
         registro.checklisted_by = request.form.get("checklisted_by")
         registro.released_by = request.form.get("released_by")
@@ -171,9 +197,8 @@ def edit_registro(id):
         registro.concerns_requests = request.form.get("concerns_requests")
         registro.scartoffie = request.form.get("scartoffie")
         registro.transazioni = request.form.get("transazioni")
-        #registro.created = request.form.get("created")
+        registro.transazioni_intl = request.form.get("transazioni_intl")
         registro.created = datetime.strptime(created_str, "%Y-%m-%d %H:%M:%S") if created_str else datetime.now()
-        #registro.last_edited = request.form.get("last_edited")
         registro.last_edited = datetime.strptime(last_edited_str, "%Y-%m-%d %H:%M:%S") if last_edited_str else datetime.now()
         registro.created_by = request.form.get("created_by")
         registro.last_edited_by = request.form.get("last_edited_by")
@@ -192,10 +217,10 @@ def edit_registro(id):
 
         try:
             db.session.commit()
-            flash("Registro updated successfully!", "success")
+            flash("Updated successfully!", "success")
         except Exception as e:
             db.session.rollback()
-            flash("Error updating registro: " + str(e), "danger")
+            flash("Error updating: " + str(e), "danger")
 
         return redirect(url_for("home_blueprint.registro"))
 
@@ -204,7 +229,7 @@ def edit_registro(id):
     makes = sorted(set([m.make for m in modelo_data]))
 
     return render_template(
-        "home/edit_registro.html",
+        "home/registro.html",
         segment="registro",
         registro=registro,
         makes=makes
